c4ddc194c57cf49d5ca1dc744c5e5847cfafee54
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-04-03 22:40:53 -0300

    Corrigindo erros tradução

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const titleServices = document.querySelector('.title-services');
 const paragraphServices = document.querySelector('.paragraph-services');
 const titleWork = document.querySelector('.title-work');
 const paragraphWork = document.querySelector('.paragraph-work');
-const btnViewMore = document.querySelector('.btnViemMore');
+const btnViewMore = document.querySelector('.btnViewMore');
 
 //----------------------------
 const br = document.createElement('BR');
@@ -57,14 +57,12 @@ const data = {
     "subTitle":"I'm a Developer Full Stack student",
     "btnAboutMe":"About Me",
     "titleContext":"I'm a Developer Full Stack student.",
-    "description":`I am passionate about technology and games. My mission is to learn more every day,
-    to improve everything around my life and the people I love.
-    I am a student of Full Stack Development at Trybe school.
-    Technology has entered my life and I hope that remaining with it remains of my life.
-    I am a student of Full Stack Development at Trybe school.
-    I am passionate about technology and games. My mission is to learn more every day,
-    to improve everything around my life and the people I love.
-    Technology has entered my life and I hope that remaining with it remains of my life.`,
+    "description":`I have been passionate about technology since I was young. 
+    I do my projects and my works with a lot of love. I am a student of Web development at the Trybe school of technology, 
+    I was able to learn more and more about the needs of the market today so 
+    I have been studying and deepening each day. I am 36 years old, have two beautiful children and a family whom I love. 
+    I am not the owner of the truth and I don't even know what awaits me in the future, but one thing is certain, 
+    the passion for my family and technology will be with me forever.`,
     "titleServices":"Main Tecnologys used.",
     "paragraphServices":"Technologies used in my daily life.",
     "btnViewMore":"VIEW MORE",
@@ -79,7 +77,12 @@ const data = {
     "subTitle":"Eu sou estudante de Desenvolvimeto Full Stack",
     "btnAboutMe":"Sobre mim",
     "titleContext":"Eu sou estudante de Desenvolvimeto Full Stack",
-    "description":"teste",
+    "description":`Sou apaixonado por tecnologia desde jovem. 
+    Eu faço os meus projetos e os meus trabalhos com muito amor. 
+    Eu sou estudante de desenvolvimento Web na escola Trybe de tecnologia, eu pude aprender cada dia mais sobre a 
+    necessidade do mercado nos dias atuais por isso eu venho estudando e me aprofundando a cada dia. Tenho 36 anos, 
+    dois filhos lindos e uma família quem eu os amo. Não sou o dono da verdade e nem sei o que me espera no futuro, 
+    mas uma coisa é certa, a paixão pela minha famía e pela tecnologia estarão comigo para sempre.`,
     "titleServices":"Principais tecnologias usadas.",
     "paragraphServices":"Tecnologias utilizadas no meu dia a dia.",
     "btnViewMore":"VEJA MAIS",
@@ -93,7 +96,12 @@ const data = {
     "subTitle": "Je suis etudiant de développeur Full Stack",
     "btnAboutMe":"À propos de moi",
     "titleContext":"Je suis etudiant de développeur Full Stack",
-    "description":"test",
+    "description":`Je suis passionné de technologie depuis mon plus jeune âge.
+    Je fais mes projets et mes œuvres avec beaucoup d'amour.
+    Je suis étudiant en développement Web à l'école de technologie Trybe, j'ai pu en apprendre de plus en plus sur le
+    nécessité du entreprises de nos jours, j'ai donc étudié et approfondi chaque jour. J'ai 36 ans,
+    deux beaux enfants et une famille que j'aime. Je ne suis pas le propriétaire de la vérité et je ne sais même pas ce qui m'attend dans le futur,
+    mais une chose est sûre, la passion pour ma famille et la technologie sera avec moi pour toujours.`,
     "titleServices":"Principales technologies utilisées.",
     "paragraphServices":"Technologies utilisées dans ma vie quotidienne.",
     "btnViewMore":"VOIR PLUS",
