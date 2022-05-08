dd8d340a2d356d6fd42712a471bedfd29f8890fb
Kaecio <kaecio@yahoo.com.br>
2022-05-08 00:15:57 -0300

    Description

--- index.js
+++ index.js
@@ -92,12 +92,17 @@ const data = {
     "subTitle":"I am Quality Assurance at Accenture",
     "btnAboutMe":"About Me",
     "titleContext":"I am Quality Assurance at Accenture.",
-    "description":`I have been passionate about technology since I was young. 
-    I do my projects and my works with a lot of love. I am a student of Web development at the Trybe school of technology, 
-    I was able to learn more and more about the needs of the market today so 
-    I have been studying and deepening each day. I am 36 years old, have two beautiful children and a family whom I love. 
-    I am not the owner of the truth and I don't even know what awaits me in the future, but one thing is certain, 
-    the passion for my family and technology will be with me forever.`,
+    "description":`I am 37 years old, two beautiful children and a family that I love.
+    I've been passionate about technology ever since.
+    I have a degree in Systems Analysis and Development from Universidade Nove de Julho – 2014.
+    
+    I started my career as Quality Assurance at Concrete Solutions in 2021. Today, I am a consultant at the largest bank in Latin America and I work in the credit card sector. I learn a lot from the partner and I still continue, working something that involves numbers, figures and customers is very challenging, but, above all, a great experience for me.
+    
+       I love to question, follow the projects from their planning, in their construction, in the delivery and of course, in their life cycle always doing tests like black box testing, white box testing, end to end, from regression, automated tests between others.
+    
+     In January 2022, Concrete had its definitive merger with Accenture do Brasil.
+        Today, I am part of Accenture as a Quality Assurance, but I continue to perform the same activity as before.
+    I do my projects and my work, I do it with a lot of love.`,
     "titleServices":"Main Tecnologys used.",
     "paragraphServices":"Technologies used in my daily life.",
     "btnViewMore":"VIEW MORE",
@@ -122,7 +127,7 @@ const data = {
     
      Em janeiro de 2022 a Concrete teve a sua junção definitiva com a Accenture do Brasil.
         Hoje, faço parte da Accenture como Quality Assurance, mas, continuo realizando a mesma atividade que fazia antes.
-    Eu faço os meus projetos e o meu trabalho, eu faço com muito amor. .`,
+    Eu faço os meus projetos e o meu trabalho, eu faço com muito amor.`,
     "titleServices":"Principais tecnologias usadas.",
     "paragraphServices":"Tecnologias utilizadas no meu dia a dia.",
     "btnViewMore":"VEJA MAIS",
@@ -137,12 +142,17 @@ const data = {
     "subTitle": "Je suis Quality Assurance chez Accenture",
     "btnAboutMe":"À propos de moi",
     "titleContext":"Je suis Quality Assurance chez Accenture",
-    "description":`Je suis passionné de technologie depuis mon plus jeune âge.
-    Je fais mes projets et mes œuvres avec beaucoup d'amour.
-    Je suis étudiant en développement Web à l'école de technologie Trybe, j'ai pu en apprendre de plus en plus sur le
-    nécessité du entreprises de nos jours, j'ai donc étudié et approfondi chaque jour. J'ai 36 ans,
-    deux beaux enfants et une famille que j'aime. Je ne suis pas le propriétaire de la vérité et je ne sais même pas ce qui m'attend dans le futur,
-    mais une chose est sûre, la passion pour ma famille et la technologie sera avec moi pour toujours.`,
+    "description":`J'ai 37 ans, deux beaux enfants et une famille que j'aime.
+    Depuis, je suis passionné par la technologie.
+    J'ai un diplôme en analyse et développement de systèmes de l'Université Nove de Julho - 2014.
+    
+    J'ai commencé ma carrière en tant qu'assurance qualité chez Concrete Solutions en 2021. Aujourd'hui, je suis consultant dans la plus grande banque d'Amérique latine et je travaille dans le secteur des cartes de crédit. J'apprends beaucoup du partenaire et je continue toujours, travailler quelque chose qui implique des chiffres, des chiffres et des clients est très difficile, mais, surtout, une belle expérience pour moi.
+    
+       J'aime remettre en question, suivre les projets depuis leur planification, dans leur construction, dans la livraison et bien sûr, dans leur cycle de vie en faisant toujours des tests comme des tests en boîte noire, des tests en boîte blanche, de bout en bout, de la régression, des tests automatisés entre autres .
+    
+     En janvier 2022, Concrete fusionne définitivement avec Accenture do Brasil.
+        Aujourd'hui, je fais partie d'Accenture en tant qu'Assurance Qualité, mais je continue à exercer la même activité qu'avant.
+    Je fais mes projets et mon travail, je le fais avec beaucoup d'amour.`,
     "titleServices":"Principales technologies utilisées.",
     "paragraphServices":"Technologies utilisées dans ma vie quotidienne.",
     "btnViewMore":"VOIR PLUS",
